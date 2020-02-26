fb23c951145bbe9cb1cdd0a7cbb9391fb5ca4fd2
Katarzyna <kkazimierczuk@gmail.com>
2020-02-26 14:50:34 +0100

    add function generateTitleLinks in js

--- js/script.js
+++ js/script.js
@@ -40,10 +40,43 @@ const titleClickHandler = function(event){
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
   }
+
   
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for (let article of articles) {
+
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log (linkHTML);
+    /* insert link into titleList */
+    html = html + linkHTML;
+    console.log ('html:', html);
+  }
+  titleList.innerHTML = html;
   const links = document.querySelectorAll('.titles a');
-  
   for(let link of links){
+
     link.addEventListener('click', titleClickHandler);
   }
+}
 
+generateTitleLinks();
